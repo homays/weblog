85f799b3aab0f4336156e9daab86cb8c4928b0ab
Arrebol <2362552845@qq.com>
2024-01-19 20:53:11 +0800

    feat: 基本模板

---
+++ weblog-admin/src/test/java/com/arrebol/admin/WeblogModuleAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arrebol.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeblogModuleAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
