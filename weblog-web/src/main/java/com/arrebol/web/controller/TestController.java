6682c4ce1921bfd2662cd8cfaec7d8525ca3e490
Arrebol <2362552845@qq.com>
2024-01-19 22:02:38 +0800

    feat: 自定义 Jackson 序列化、反序列化，支持 Java 8 日期新特性

--- weblog-web/src/main/java/com/arrebol/web/controller/TestController.java
+++ weblog-web/src/main/java/com/arrebol/web/controller/TestController.java
@@ -1,9 +1,20 @@
 package com.arrebol.web.controller;
 
 import com.arrebol.common.aspect.ApiOperationLog;
+import com.arrebol.common.util.JsonUtil;
+import com.arrebol.common.util.Result;
+import com.arrebol.web.entity.User;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 /**
  * Description
@@ -12,11 +23,20 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2024/1/19
  */
 @RestController
+@Slf4j
 public class TestController {
-    @GetMapping("/test")
+    @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
-    public String test() {
-        // 返参
-        return "hello world";
+    @ApiOperation(value = "测试接口")
+    public Result test(@RequestBody @Validated User user) {
+        // 打印入参
+        log.info(JsonUtil.toJsonString(user));
+
+        // 设置三种日期字段值
+        user.setCreateTime(LocalDateTime.now());
+        user.setUpdateDate(LocalDate.now());
+        user.setTime(LocalTime.now());
+
+        return Result.success(user);
     }
 }
