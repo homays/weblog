191bd5cc1a831dd338e9e52b333852252e3a73b7
Arrebol <2362552845@qq.com>
2024-01-21 14:55:23 +0800

    feat: 整合 Spring Security

--- weblog-web/src/main/java/com/arrebol/web/controller/TestController.java
+++ weblog-web/src/main/java/com/arrebol/web/controller/TestController.java
@@ -25,7 +25,7 @@ import java.time.LocalTime;
 @RestController
 @Slf4j
 public class TestController {
-    @PostMapping("/test")
+    @PostMapping("/admin/test")
     @ApiOperationLog(description = "测试接口")
     @ApiOperation(value = "测试接口")
     public Result test(@RequestBody @Validated User user) {
