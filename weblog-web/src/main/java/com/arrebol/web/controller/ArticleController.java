1a90a83314ecd7335e8904f4913a090db184be82
Arrebol <2362552845@qq.com>
2024-02-28 16:41:42 +0800

    fix: 请求路经修改

--- weblog-web/src/main/java/com/arrebol/web/controller/ArticleController.java
+++ weblog-web/src/main/java/com/arrebol/web/controller/ArticleController.java
@@ -11,17 +11,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Api(tags = "文章")
+@RequestMapping("/article")
 @RequiredArgsConstructor
 public class ArticleController {
 
     @Autowired
     private ArticleService articleService;
 
-    @PostMapping("/article/list")
+    @PostMapping("/list")
     @ApiOperation(value = "获取首页文章分页数据")
     @ApiOperationLog(description = "获取首页文章分页数据")
     public Response findArticlePageList(@RequestBody FindIndexArticlePageListReqVO findIndexArticlePageListReqVO) {
